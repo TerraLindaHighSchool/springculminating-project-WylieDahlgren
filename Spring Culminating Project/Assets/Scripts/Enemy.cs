731275acf6900a1b521a258b76d9f7b87cfd46a1
Wylie Dahlgren <wd00833@srcs.loc>
2020-03-05 15:33:07 -0800

    Started 1.1

---
+++ Spring Culminating Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (transform.position - player.transform.position).normalized;
+        float yAngle = Mathf.Atan2(lookDirection.x, lookDirection.z) * 180 / Mathf.PI;
+        transform.Translate(lookDirection * Time.deltaTime * speed);
+        transform.Rotate(0, yAngle, 0);
+        Debug.Log(yAngle);
+    }
+}
---
+++ Spring Culminating Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+    public float speedX = 13.0f;
+    public float xRange = 30;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z < -xRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -xRange);
+        }
+        if (transform.position.z > xRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, xRange);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        if(Input.GetKey(KeyCode.R))
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speedX);
+            transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speedX);
+        }
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
